021d9dc406cb6a77fcdfcd569024f82393af8e33
2016lisali <2016lisali@gmai.com>
2022-02-02 22:40:15 +1000

    modify reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import posts from './posts'
+import reducer from './posts'
 
 
-export default combineReducers({ posts })
+export default combineReducers({ reducer })
 // * in an object, if the key and the value are same, can only keep one.
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,9 +1,9 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes'
 
-export default (posts = [], action) => {
+const reducer = (posts = [], action) => {
   switch (action.type) {
     case DELETE:
-      return posts.filter(post => post._id != action.payload);
+      return posts.filter(post => post._id !== action.payload);
     case UPDATE: 
       return posts.map((post)=> post._id === action.payload._id ? action.payload : post)
     case FETCH_ALL:
@@ -14,3 +14,5 @@ export default (posts = [], action) => {
       return posts;
   }
 }
+
+export default reducer;
