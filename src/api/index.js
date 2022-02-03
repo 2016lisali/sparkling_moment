18fe8f1c3ef21a5a6fbbe5f28bd14a85b0dbe78a
2016lisali <2016lisali@gmai.com>
2022-02-03 11:53:17 +1000

    change postReducer to posts in reducer/index.js file which caused data undefined  in posts/posts.js file

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,7 @@ import axios from 'axios';
 const url = 'https://sparkling-moment.herokuapp.com/posts';
 
 // export const fetchPosts = ()=> axios.get(url);
-export const fetchPosts = () => axios({
-  method: 'get',
-  url: url,
-  responseType: 'stream'
-})
-
+export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`)
